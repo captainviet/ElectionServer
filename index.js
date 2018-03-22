ffb21781e9fc00870b6c2ae2c515043f1d4fdaa3
Vince Dang <xuanvu001@e.ntu.edu.sg>
2018-03-23 02:23:23 +0800

    Enable CORS built-in

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const mongo = require('koa-mongo')
 const logger = require('koa-logger')
+const cors = require('@koa/cors')
 
 const router = require('app/router')
 
 const app = new Koa()
 app.use(logger())
+app.use(cors())
 
 app.use(bodyParser())
 app.use(mongo({
