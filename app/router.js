333d8a67162aac8e2e21e0b173e16005b98a45af
Vince Dang <xuanvu001@e.ntu.edu.sg>
2018-03-24 11:30:41 +0700

    Implement API endpoints wire-by-configuration

--- app/router.js
+++ app/router.js
@@ -3,15 +3,40 @@ const Router = require('koa-router')
 const VoteHandler = require('app/vote/controller')
 const CandidateHandler = require('app/candidate/controller')
 
+const routeOptions = require('app/expose/options')
+const { candidate, vote } = routeOptions.registry
+
+const Routes = {}
+Routes[candidate.list] = CandidateHandler.list
+Routes[candidate.add] = CandidateHandler.add
+Routes[candidate.eradicate] = CandidateHandler.eradicate
+Routes[vote.list] = VoteHandler.list
+Routes[vote.exists] = VoteHandler.exists
+Routes[vote.submit] = VoteHandler.submit
+Routes[vote.eradicate] = VoteHandler.eradicate
+
 const config = require('app/config')
+const appConfig = config.app
+const env = appConfig.expose.env
+const domain = appConfig.expose.domain
+const allowedRoutes = routeOptions[env][domain]
 
 const router = new Router()
-router.get('/candidate/list/', CandidateHandler.list)
-router.post('/candidate/add/', CandidateHandler.add)
-router.post('/candidate/eradicate/', CandidateHandler.eradicate)
-router.get('/vote/list/', VoteHandler.list)
-router.get('/vote/exists/:name', VoteHandler.exists)
-router.post('/vote/submit/', VoteHandler.submit)
-router.post('/vote/eradicate/', VoteHandler.eradicate)
+allowedRoutes.forEach(route => {
+  const { path, method } = route
+  const handler = Routes[path]
+  if (handler) {
+    switch (method) {
+      case 'get':
+        router.get(path, handler)
+        break
+      case 'post':
+        router.post(path, handler)
+        break
+      default:
+        console.log(route)
+    }
+  }
+})
 
 module.exports = router
